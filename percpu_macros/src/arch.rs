fcd8ad04401b63c0b61fe7dde8aac7ebb858efdd
yfblock <321353225@qq.com>
2025-01-24 17:04:22 +0800

    feat: Add loongarch64 support

--- percpu_macros/src/arch.rs
+++ percpu_macros/src/arch.rs
@@ -37,6 +37,13 @@ pub fn gen_offset(symbol: &Ident) -> proc_macro2::TokenStream {
                 out(reg) value,
                 VAR = sym #symbol,
             );
+            #[cfg(any(target_arch = "loongarch64"))]
+            ::core::arch::asm!(
+                "lu12i.w {0}, %abs_hi20({VAR})",
+                "ori {0}, {0}, %abs_lo12({VAR})",
+                out(reg) value,
+                VAR = sym #symbol,
+            );
             value
         }
     }
@@ -73,6 +80,8 @@ pub fn gen_current_ptr(symbol: &Ident, ty: &Type) -> proc_macro2::TokenStream {
             ::core::arch::asm!(#aarch64_asm, out(reg) base);
             #[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
             ::core::arch::asm!("mv {}, gp", out(reg) base);
+            #[cfg(any(target_arch = "loongarch64"))]
+            ::core::arch::asm!("move {}, $r21", out(reg) base);
             (base + self.offset()) as *const #ty
         }
     })
@@ -98,6 +107,26 @@ pub fn gen_read_current_raw(symbol: &Ident, ty: &Type) -> proc_macro2::TokenStre
             "lui {0}, %hi({VAR})",
             "add {0}, {0}, gp",
             concat!(#rv64_op, " {0}, %lo({VAR})({0})"),
+            out(reg) value,
+            VAR = sym #symbol,
+        )
+    };
+
+    // https://loongson.github.io/LoongArch-Documentation/LoongArch-Vol1-EN.html#_ldx_buhuwud_stx_bhwd
+    let la64_op = match ty_str.as_str() {
+        "bool" => "ldx.bu",
+        "u8" => "ldx.bu",
+        "u16" => "ldx.hu",
+        "u32" => "ldx.wu",
+        "u64" => "ldx.d",
+        "usize" => "ldx.d",
+        _ => unreachable!(),
+    };
+    let la64_asm = quote! {
+        ::core::arch::asm!(
+            "lu12i.w {0}, %abs_hi20({VAR})",
+            "ori {0}, {0}, %abs_lo12({VAR})",
+            concat!(#la64_op, " {0}, {0}, $r21"),
             out(reg) value,
             VAR = sym #symbol,
         )
@@ -142,13 +171,16 @@ pub fn gen_read_current_raw(symbol: &Ident, ty: &Type) -> proc_macro2::TokenStre
     };
 
     let rv64_code = gen_code(rv64_asm);
+    let la64_code = gen_code(la64_asm);
     let x64_code = gen_code(x64_asm);
     macos_unimplemented(quote! {
         #[cfg(target_arch = "riscv64")]
         { #rv64_code }
+        #[cfg(target_arch = "loongarch64")]
+        { #la64_code }
         #[cfg(target_arch = "x86_64")]
         { #x64_code }
-        #[cfg(not(any(target_arch = "riscv64", target_arch = "x86_64")))]
+        #[cfg(not(any(target_arch = "riscv64", target_arch = "loongarch64", target_arch = "x86_64")))]
         { *self.current_ptr() }
     })
 }
@@ -185,6 +217,27 @@ pub fn gen_write_current_raw(symbol: &Ident, val: &Ident, ty: &Type) -> proc_mac
         );
     };
 
+    // https://loongson.github.io/LoongArch-Documentation/LoongArch-Vol1-EN.html#common-memory-access-instructions
+    let la64_op = match ty_str.as_str() {
+        "bool" => "stx.b",
+        "u8" => "stx.b",
+        "u16" => "stx.h",
+        "u32" => "stx.w",
+        "u64" => "stx.d",
+        "usize" => "stx.d",
+        _ => unreachable!(),
+    };
+    let la64_code = quote! {
+        ::core::arch::asm!(
+            "lu12i.w {0}, %abs_hi20({VAR})",
+            "ori {0}, {0}, %abs_lo12({VAR})",
+            concat!(#la64_op, " {1}, {0}, $r21"),
+            out(reg) _,
+            in(reg) #val as #ty_fixup,
+            VAR = sym #symbol,
+        );
+    };
+
     let (x64_asm, x64_reg) = if ["bool", "u8"].contains(&ty_str.as_str()) {
         (
             "mov byte ptr gs:[offset {VAR}], {0}".into(),
@@ -210,9 +263,11 @@ pub fn gen_write_current_raw(symbol: &Ident, val: &Ident, ty: &Type) -> proc_mac
     macos_unimplemented(quote! {
         #[cfg(target_arch = "riscv64")]
         { #rv64_code }
+        #[cfg(target_arch = "loongarch64")]
+        { #la64_code }
         #[cfg(target_arch = "x86_64")]
         { #x64_code }
-        #[cfg(not(any(target_arch = "riscv64", target_arch = "x86_64")))]
+        #[cfg(not(any(target_arch = "riscv64", target_arch = "loongarch64", target_arch = "x86_64")))]
         { *(self.current_ptr() as *mut #ty) = #val }
     })
 }
