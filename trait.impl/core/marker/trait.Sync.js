3f48a07d854bc17a69c9f23d88fdf5da7a6f74bf
equation314 <equation618@gmail.com>
2025-01-24 13:59:35 +0000

    Deploying to gh-pages from @ arceos-org/percpu@9d7e827b66e755b27bb66207bf70b17a9847029f 🚀

---
+++ trait.impl/core/marker/trait.Sync.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["percpu",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"percpu/struct.EXAMPLE_PERCPU_DATA_WRAPPER.html\" title=\"struct percpu::EXAMPLE_PERCPU_DATA_WRAPPER\">EXAMPLE_PERCPU_DATA_WRAPPER</a>",1,["percpu::EXAMPLE_PERCPU_DATA_WRAPPER"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[362]}
